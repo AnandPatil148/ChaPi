4b80c32d293cb37671fc0dca619b09962b150363
LiveGamerz <85071760+AnandPatil148@users.noreply.github.com>
2022-08-09 14:23:48 +0530

    2 clients connections

--- client.py
+++ client.py
@@ -13,26 +13,37 @@ NAME = str(input('Name:'))
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg, name):
-    Ename = name.encode(FORMAT)
-    name_length = len(Ename)
-    send_Nlength = str(name_length).encode(FORMAT)
-    send_Nlength += b' ' * (HEADER - len(send_Nlength))
-
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-
-    client.send(send_Nlength)
-    client.send(Ename)
-    client.send(send_length)
-    client.send(message)
+def server_receive():
+    while True:
+        try:
+            message = client.recv(HEADER).decode(FORMAT)
+            print(message)
+        except:
+            print("error")
+            client.close()
+            break
 
 
-while True:
-    MSG = input ('%s: '%(NAME))
-    if MSG == DISCONNECT_MESSAGE:
-        connected = False
-    send(MSG, NAME)
+def send():
+    while True:
+        MSG = input (f'{NAME}: ')
+        
+        Ename = NAME.encode(FORMAT)
+        name_length = len(Ename)
+        send_Nlength = str(name_length).encode(FORMAT)
+        send_Nlength += b' ' * (HEADER - len(send_Nlength))
 
+        message = MSG.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+
+        client.send(send_Nlength)
+        client.send(Ename)
+        client.send(send_length)
+        client.send(message)
+
+        if MSG == DISCONNECT_MESSAGE:
+            client.close()
+            break
+        
--- server.py
+++ server.py
@@ -3,34 +3,45 @@
 import socket 
 import threading
 
+
 HEADER = 64
 PORT = 5050
 SERVER = input('Server IP:')
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!dc"
+CLIENTS = []
+NAMES = []
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+def broadcast(message):
+    for conn in CLIENTS:
+        conn.send(message.encode(FORMAT)) #3rd conn.send
 
+def handle_client(conn, addr):
     connected = True
     while connected:
-        name_lenght = conn.recv(HEADER).decode(FORMAT)
-        if name_lenght:
-                name_lenght = int(name_lenght)
-                name = conn.recv(name_lenght).decode(FORMAT)
-
+        
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
+        
+        broadcast(f'{name} has connected'.encode(FORMAT))
+        print(f"Name of {addr} is {name}")
                 
-        print(f"[{addr}{name}] {msg}")
+        broadcast(msg)
+
+        print(f"[{addr}] {msg}")
         if msg == DISCONNECT_MESSAGE:
-            print (f"[{name}] DISCONNECTED")
+            index = CLIENTS.index(conn)
+            name = NAMES[index]
+            broadcast(f'{name} has disconnected'.encode(FORMAT))
+            print (f"[{addr}] DISCONNECTED")
+            CLIENTS.remove(conn)
+            NAMES.remove(name)
             connected = False
 
     conn.close()
@@ -41,8 +52,22 @@ def start():
     print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
     while True:
         conn, addr = server.accept()
+        print(f"[NEW CONNECTION] {addr} connected.")
+
+        name_lenght = conn.recv(HEADER).decode(FORMAT)
+        if name_lenght:
+            name_lenght = int(name_lenght)
+            name = conn.recv(name_lenght).decode(FORMAT)
+
+        CLIENTS.append(conn) 
+        NAMES.append(name)
+
+        broadcast(f'{name} has connected')
+        print(f"Name of {addr} is {name}")
+
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 
